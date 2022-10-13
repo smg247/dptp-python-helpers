0ad5305b48dab883d47280f50f3fd0cf367297bf
Stephen Goeddel <sgoeddel@redhat.com>
2022-10-13 15:02:35 -0400

    update script to grab post and reply count for users

--- slack.py
+++ slack.py
@@ -30,7 +30,9 @@ class Message:
             self.day = self.time.day
 
     def __str__(self) -> str:
-        return "Username: " + str(self.user) + "\n" + "Bot ID: " + str(self.bot_id) + "\n" + "Reply Count: " + str(self.reply_count) + "\n" + "Reply Users: " + str(self.reply_users_count) + "\n" + "Timestamp: " + str(self.time) + "\n"
+        return "Username: " + str(self.user) + "\n" + "Bot ID: " + str(self.bot_id) + "\n" + "Reply Count: " + str(
+            self.reply_count) + "\n" + "Reply Users: " + str(self.reply_users_count) + "\n" + "Timestamp: " + str(
+            self.time) + "\n"
 
     def to_dataframe_list(self) -> []:
         return [self.user, self.bot_id, self.reply_count, self.reply_users_count, self.month, self.day, self.year]
@@ -63,15 +65,101 @@ def retrieve_messages(month) -> []:
     return ret
 
 
-def convert_to_dataframe(messages) -> pandas.DataFrame:
+class UserMessageCount:
+
+    def __init__(self, user, post_count, reply_count) -> None:
+        self.user = user
+        self.name = ''
+        self.email = ''
+        self.full_name = ''
+        self.post_count = post_count
+        self.reply_count = reply_count
+
+    def __str__(self) -> str:
+        return "User Id: " + str(self.user) + "\n" + "Name:" + str(self.name) + "\n" + "Post Count: " + str(
+            self.post_count) + "\n" + "Reply Count: " + str(self.reply_count)
+
+
+def get_user_message_counts(start) -> {}:
+    client = WebClient(token=os.environ.get("OAUTH_TOKEN"))
+
+    users = {}
+    next_cursor = None
+    while True:
+        history = client.conversations_history(channel=FORUM_CHANNEL,
+                                               limit=1000,
+                                               cursor=next_cursor,
+                                               oldest=str(start.timestamp()))
+        metadata = history.data.get('response_metadata')
+        if metadata:
+            next_cursor = metadata.get("next_cursor")
+        else:
+            next_cursor = None
+
+        for message in history.data.get('messages'):
+            if message.get('subtype') == 'bot_message':
+                poster_id = None
+                # Attempt to get the original poster of this workflow
+                blocks = message.get('blocks')
+                if len(blocks) > 0:
+                    elements = blocks[0].get('elements')
+                    if elements and len(elements) > 0:
+                        inner_elements = elements[0].get('elements')
+                        for e in inner_elements:
+                            poster_id = e.get('user_id')
+                            if poster_id:
+                                user = users.get(poster_id)
+                                if user:
+                                    user.post_count += 1
+                                else:
+                                    user = UserMessageCount(user=poster_id, post_count=1, reply_count=0)
+                                users[poster_id] = user
+                                break
+
+                # Get all users who replied to this workflow (other than the original poster)
+                reply_users = message.get('reply_users')
+                if reply_users:
+                    for user_id in reply_users:
+                        # We don't care about bots here so make sure it's an actual user
+                        if user_id.startswith('U') and user_id != poster_id:
+                            user = users.get(user_id)
+                            if user:
+                                user.reply_count += 1
+                            else:
+                                user = UserMessageCount(user=user_id, post_count=0, reply_count=1)
+                            users[user_id] = user
+
+        if next_cursor is None:
+            break
+
+    for user_id in users.keys():
+        user_info = client.users_info(user=user_id)
+        info = user_info.data.get('user')
+        user = users.get(user_id)
+        user.name = info.get('name')
+        user.email = info.get('profile').get('email')
+        user.full_name = info.get('real_name')
+
+    return users
+
+
+def convert_messages_to_dataframe(messages) -> pandas.DataFrame:
     fields = ['user', 'bot_id', 'reply_count', 'reply_users_count', 'month', 'day', 'year']
     return pd.DataFrame([{fn: getattr(m, fn) for fn in fields} for m in messages])
 
 
+def convert_users_to_dataframe(users) -> pandas.DataFrame:
+    fields = ['user', 'name', 'email', 'full_name', 'post_count', 'reply_count']
+    return pd.DataFrame([{fn: getattr(u, fn) for fn in fields} for u in users])
+
+
 if __name__ == '__main__':
-    messages = retrieve_messages(datetime(2022, 9, 1))
-    data_frame = convert_to_dataframe(messages)
+    # messages = retrieve_messages(datetime(2022, 10, 1))
+    # data_frame = convert_messages_to_dataframe(messages)
+    # print(data_frame)
+    # data_frame.to_clipboard(index=False, header=False)
+
+    user_message_counts = get_user_message_counts(datetime(2022, 6, 1))
+    data_frame = convert_users_to_dataframe(user_message_counts.values())
     print(data_frame)
     data_frame.to_clipboard(index=False)
-
-
